77489d17628448ac930fd4111d2c898feb6a187a
ja180943 <NaomiMonAmour3>
2022-12-31 18:15:12 +0100

    Import des fixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -9,18 +9,60 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager):void
     {
-        $sale = new Sale();
 
-            $csv = fopen(dirname(__FILE__).'TODO PATH ', 'r');
+            $arrayRegion= array();
+            $csvregion = fopen( dirname(__DIR__) . "/Data/regions.csv",'r');
 
+           while (!feof($csvregion)) {
+                $lineregion = fgetcsv($csvregion,1024,";");
+                if($lineregion!=false)
+                {
+                    $arrayRegion[$lineregion[0]] = $lineregion[1];
+                }
+           }
 
+           fclose($csvregion);
+
+            $csv = fopen( dirname(__DIR__).'/Data/valeurs-2018.csv', 'r');
             $i = 0;
 
+            // 0 : PRIX | 1 : ZIP | 2: AREA | 3 : Date | 4 TYPE
             while (!feof($csv)) {
-                $line = fgetcsv($csv);
-                /* TODO Import */
-                //$sale->setMachin($line[0]);
-                //$manager->persist();
+
+               $sale = new Sale();
+               $line = fgetcsv($csv,1024,";");
+               if($line!=false)
+                {
+                    print_r($line);
+                    /* TODO Import */
+                    $sale->setPrice(floatval($line[0]));
+                    $sale->setArea(floatval($line[2]));
+                    $sale->setZipCode($line[1]);
+                    $dep = "";
+
+                    if(strlen($line[1])== 4)
+                    {
+                        $dep = "0" . substr($line[1],0,1);
+                    }
+                    else
+                    {
+                        $dep = substr($line[1],0,2);
+                        if($dep=="97"){
+                            $dep=substr($line[1],0,3);
+                        }
+                    }
+                    if($dep=="")
+                        continue;
+                    $sale->setRegion($arrayRegion[$dep]);
+                    $dateExp = explode("/",$line[3]);
+                    $sale->setDate(new \DateTime($dateExp[2]."-".$dateExp[1]."-".$dateExp[0]));
+                    if($line[4]==1)
+                        $sale->setType("Maison");
+                    else
+                        $sale->setType("Appartement");
+
+                    $manager->persist($sale);
+                }
             }
 
             fclose($csv);
