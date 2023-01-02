50bc16cf5d6f42e3307651ca02942175da144a3b
NAOMI LAPLACE <naomi.laplace@etu.univ-lehavre.fr>
2023-01-02 17:13:42 +0100

    Style Global

--- pwa/pages/index.tsx
+++ pwa/pages/index.tsx
@@ -1,19 +1,55 @@
 import Head from "next/head";
 import Link from "next/link";
 import React from "react";
-
+import bg from "../public/background.jpg";
 import {PriceByArea} from "../components/PriceByArea";
+import {SalesByMonth} from "../components/SalesByMonth";
+import {SalesByRegion} from "../components/SalesByRegion";
 
 const Welcome = () => (
   <>
     <Head>
-      <title>Sales stats</title>
+      <title>Statistiques</title>
     </Head>
 
-    <div>
-      <PriceByArea />
-    </div>
 
+    <header className="titre fixed-top">
+      <div>
+        <h3>Statistiques ventes immobilières</h3>
+        <nav className="navbar navbar-expand-lg navbar-light">
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link active" href="#graph1">Prix au mètre carré</a>
+              <a className="nav-item nav-link active" href="#graph2">Ventes par année</a>
+              <a className="nav-item nav-link active" href="#">Ventes par région</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+
+
+    <div className="corps">
+      <div id="graph1" className="card border-secondary mb-3">
+        <div className="card-img-top graph">
+          <PriceByArea />
+        </div>
+        <div className="card-body titleGraph">
+          <h5 className="card-title">Prix au mètre carré</h5>
+        </div>
+      </div>
+
+      <div id="graph2" className="card border-secondary mb-3">
+        <div className="card-img-top graph">
+          <SalesByMonth />
+        </div>
+        <div className="card-body titleGraph">
+          <h5 className="card-title">Ventes par années</h5>
+        </div>
+      </div>
+
+
+    </div>
   </>
 );
 
