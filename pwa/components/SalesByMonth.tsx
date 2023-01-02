47c6042ea2f2e4a5dd19c686d6c308be5cde8b37
NAOMI LAPLACE <naomi.laplace@etu.univ-lehavre.fr>
2023-01-02 22:59:35 +0100

    Données pour les ventes par mois

--- pwa/components/SalesByMonth.tsx
+++ pwa/components/SalesByMonth.tsx
@@ -1,18 +1,62 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import * as d3 from "react-d3-components";
 var BarChart = d3.BarChart;
-export const SalesByMonth: () => JSX.Element = () => {
-
-  var data = [{
+export const SalesByMonth: () => any = () => {
+  const [year, setYear] = React.useState(2018);
+  const [data, setData] = React.useState([{
     label:'somethingA',
-    values:[{x:'SomethingA',y:10},{x:'SomethingB',y:4},{x:'SomethingC',y:3}]
-  }];
+    values:[{x:'SomethingA',y:10},{x:'SomethingB',y:4},{x:'SomethingC',y:3}]}]);
+
+  useEffect(() => {
+    async function getData(year){
+
+      await fetchData(year).then((response) => {
+        return response.json();
+      }).then((json) => {
+        let lejson = json["hydra:member"];
+        console.log(lejson);
+        setData([{ label:'somethingA', values:lejson}]);
+      })
+
+    }
+
+    getData(year);
+  },[year])
+
+  function handleClick(e) {
+    console.log("Changement année");
+    setYear(e);
+  }
+
   return (
-    <BarChart
-      data={data}
-      width={600}
-      height={300}
-      margin={{top:10,bottom:50,left:50,right:10}}/>
+    <div>
+      <div>
+        <div className="btn-group btn-group-toggle" data-toggle="buttons">
+          <label className="btn btn-secondary">
+            <input hidden type="radio" name="options" id="option1" onClick={() => handleClick(2018)} value="2018" />2018
+          </label>
+          <label className="btn btn-secondary">
+            <input hidden type="radio" name="options" id="option2" onClick={() => handleClick(2019)} value="2019" />2019
+          </label>
+          <label className="btn btn-secondary">
+            <input hidden type="radio" name="options" id="option3" onClick={() => handleClick(2020)} value="2020"/>2020
+          </label>
+        </div>
+
+
+      </div>
+
+      <BarChart
+        data={data}
+        width={900}
+        height={300}
+        margin={{top:50,bottom:50,left:100,right:50}}/>
+    </div>
   )
+}
+
+const fetchData = async function (year) {
+  let res = await fetch("https://localhost/countbymonth/"+ year + "-01/" + year + "-12");
+  return res;
 }
 
