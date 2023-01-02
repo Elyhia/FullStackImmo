584a89173608d82a3cfffbbe80482df06fc5c7a0
NAOMI LAPLACE <naomi.laplace@etu.univ-lehavre.fr>
2023-01-02 21:07:23 +0100

    Récupération des données pour le graphique prix au m2

--- pwa/components/PriceByArea.tsx
+++ pwa/components/PriceByArea.tsx
@@ -1,18 +1,53 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import * as d3 from "react-d3-components";
 var LineChart = d3.LineChart;
-export const PriceByArea: () => JSX.Element = () => {
+export const PriceByArea: () => any = () => {
+  const [year, setYear] = React.useState(2018);
+  const [data, setData] = React.useState([{
+      label:'somethingA',
+      values:[{x:'SomethingA',y:10},{x:'SomethingB',y:4},{x:'SomethingC',y:3}]}]);
 
-  var data = [{
-    label:'somethingA',
-    values:[{x:'SomethingA',y:10},{x:'SomethingB',y:4},{x:'SomethingC',y:3}]
-  }];
+  function handleClick(e) {
+    setYear(e);
+    fetchData(year);
+  }
+
+  useEffect(() => {
+    async function getData(year){
+
+      await fetchData(year).then((response) => {
+        return response.json();
+      }).then((json) => {
+        let lejson = json["hydra:member"];
+        console.log(lejson);
+        setData([{ label:'somethingA', values:lejson}]);
+      })
+
+    }
+
+    getData(year);
+  },[])
+
   return (
-    <LineChart
+    <div>
+      <div>
+        <button onClick={() => handleClick(2018)} value="2018">2018</button>
+        <button onClick={() => handleClick(2019)} value="2019">2019</button>
+        <button onClick={() => handleClick(2020)} value="2020">2020</button>
+        <button onClick={() => handleClick(2021)} value="2021">2021</button>
+      </div>
+
+      <LineChart
       data={data}
-      width={600}
+      width={900}
       height={300}
-      margin={{top:10,bottom:50,left:50,right:10}}/>
+      margin={{top:50,bottom:50,left:100,right:50}}/>
+    </div>
   )
+}
+
+const fetchData = async function (year) {
+  let res = await fetch("https://localhost/pricebyarea/"+ year);
+  return res;
 }
 
